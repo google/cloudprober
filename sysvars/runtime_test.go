0c6c43957ad1dce361f0b5ad3d7054850c9553ad
Manu Garg <manugarg@gmail.com>
2017-12-15 14:28:10 -0800

    Add a few Go runtime stats.
    Add a few Go runtime stats to help with debugging cloudprober performance/health.
    
    cloudprober 1513308467341380164 1513308517 labels=ptype=sysvars,probe=sysvars uptime=50 gc_time=1827920 mallocs=80363 frees=39703 cpu_usage=690762494
    cloudprober 1513308467341380185 1513308567 labels=ptype=sysvars,probe=sysvars goroutines=161 mem_stats_sys=17836280
    
    ORIGINAL_AUTHOR=Manu Garg <manugarg@gmail.com>
    PiperOrigin-RevId: 179240256

---
+++ sysvars/runtime_test.go
@@ -0,0 +1,60 @@
+package sysvars
+
+import (
+	"runtime"
+	"testing"
+	"time"
+
+	"github.com/google/cloudprober/logger"
+	"github.com/google/cloudprober/metrics"
+)
+
+func TestCounterRuntimeVars(t *testing.T) {
+	dataChan := make(chan *metrics.EventMetrics, 1)
+	l := &logger.Logger{}
+	m := &runtime.MemStats{}
+	runtime.ReadMemStats(m)
+	ts := time.Now()
+
+	counterRuntimeVars(dataChan, ts, m, l)
+	em := <-dataChan
+
+	if em.Timestamp != ts {
+		t.Errorf("em.Timestamp=%v, want=%v", em.Timestamp, ts)
+	}
+
+	if em.Kind != metrics.CUMULATIVE {
+		t.Errorf("Metrics kind is not cumulative.")
+	}
+
+	for _, name := range []string{"uptime", "uptime_msec", "gc_time_msec", "mallocs", "frees", "cpu_usage_msec"} {
+		if em.Metric(name) == nil {
+			t.Errorf("Expected metric \"%s\" not defined in EventMetrics: %s", name, em.String())
+		}
+	}
+}
+
+func TestGaugeRuntimeVars(t *testing.T) {
+	dataChan := make(chan *metrics.EventMetrics, 1)
+	l := &logger.Logger{}
+	m := &runtime.MemStats{}
+	runtime.ReadMemStats(m)
+	ts := time.Now()
+
+	gaugeRuntimeVars(dataChan, ts, m, l)
+	em := <-dataChan
+
+	if em.Timestamp != ts {
+		t.Errorf("em.Timestamp=%v, want=%v", em.Timestamp, ts)
+	}
+
+	if em.Kind != metrics.GAUGE {
+		t.Errorf("Metrics kind is not gauge.")
+	}
+
+	for _, name := range []string{"goroutines", "mem_stats_sys_bytes"} {
+		if em.Metric(name) == nil {
+			t.Errorf("Expected metric \"%s\" not defined in EventMetrics: %s", name, em.String())
+		}
+	}
+}
