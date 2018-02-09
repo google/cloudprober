84e86a6200b00be6408085504954f003d806a543
Manu Garg <manugarg@gmail.com>
2018-02-08 21:53:45 -0800

    Cloudprober extensions.
    Create a system to extend cloudprober through protobuf extensions.
    
    ORIGINAL_AUTHOR=Manu Garg <manugarg@gmail.com>
    PiperOrigin-RevId: 185098351

--- probes/probes.go
+++ probes/probes.go
@@ -19,11 +19,14 @@ package probes
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"regexp"
 	"sync"
 	"time"
 
 	"github.com/golang/glog"
+	"github.com/golang/protobuf/proto"
 	"github.com/google/cloudprober/logger"
 	"github.com/google/cloudprober/metrics"
 	"github.com/google/cloudprober/probes/dns"
@@ -43,6 +46,8 @@ const (
 var (
 	userDefinedProbes   = make(map[string]Probe)
 	userDefinedProbesMu sync.Mutex
+	extensionMap        = make(map[int]func() Probe)
+	extensionMapMu      sync.Mutex
 )
 
 func runOnThisHost(runOn string, hostname string) bool {
@@ -74,6 +79,34 @@ func newLogger(probeName string) (*logger.Logger, error) {
 	return logger.New(context.Background(), logsNamePrefix+"."+probeName)
 }
 
+func getExtensionProbe(p *ProbeDef) (Probe, interface{}, error) {
+	extensions := proto.RegisteredExtensions(p)
+	if len(extensions) > 1 {
+		return nil, nil, fmt.Errorf("only one probe extension is allowed per probe, got %d extensions", len(extensions))
+	}
+	var field int
+	var desc *proto.ExtensionDesc
+	// There should be only one extension.
+	for f, d := range extensions {
+		field = int(f)
+		desc = d
+	}
+	if desc == nil {
+		return nil, nil, errors.New("no probe extension in probe config")
+	}
+	value, err := proto.GetExtension(p, desc)
+	if err != nil {
+		return nil, nil, err
+	}
+	extensionMapMu.Lock()
+	defer extensionMapMu.Unlock()
+	newProbeFunc, ok := extensionMap[field]
+	if !ok {
+		return nil, nil, fmt.Errorf("no probes registered for the extension: %d", field)
+	}
+	return newProbeFunc(), value, nil
+}
+
 // Init initializes the probes defined in the config.
 func Init(probeProtobufs []*ProbeDef, globalTargetsOpts *targets.GlobalTargetsOptions, sysVars map[string]string) map[string]Probe {
 	globalTargetsLogger, err := newLogger("globalTargets")
@@ -93,7 +126,6 @@ func Init(probeProtobufs []*ProbeDef, globalTargetsOpts *targets.GlobalTargetsOp
 			Interval: time.Duration(p.GetIntervalMsec()) * time.Millisecond,
 			Timeout:  time.Duration(p.GetTimeoutMsec()) * time.Millisecond,
 		}
-		var err error
 		if opts.Logger, err = newLogger(p.GetName()); err != nil {
 			glog.Exitf("Error in initializing logger for the probe %s. Err: %v", p.GetName(), err)
 		}
@@ -144,10 +176,16 @@ func initProbe(p *ProbeDef, opts *options.Options) (probe Probe) {
 	case ProbeDef_UDP_LISTENER:
 		probe = &udplistener.Probe{}
 		opts.ProbeConf = p.GetUdpListenerProbe()
+	case ProbeDef_EXTENSION:
+		var err error
+		probe, opts.ProbeConf, err = getExtensionProbe(p)
+		if err != nil {
+			glog.Exit(err.Error())
+		}
 	case ProbeDef_USER_DEFINED:
 		userDefinedProbesMu.Lock()
 		defer userDefinedProbesMu.Unlock()
-		probe := userDefinedProbes[p.GetName()]
+		probe = userDefinedProbes[p.GetName()]
 		if probe == nil {
 			glog.Exitf("unregistered user defined probe: %s", p.GetName())
 		}
@@ -161,7 +199,8 @@ func initProbe(p *ProbeDef, opts *options.Options) (probe Probe) {
 	return
 }
 
-// Register allows you to register a user defined probe with cloudprober.
+// RegisterUserDefined allows you to register a user defined probe with
+// cloudprober.
 // Example usage:
 //	import (
 //		"github.com/google/cloudprober"
@@ -169,13 +208,23 @@ func initProbe(p *ProbeDef, opts *options.Options) (probe Probe) {
 //	)
 //
 //	p := &FancyProbe{}
-//	probes.Register("fancy_probe", p)
+//	probes.RegisterUserDefined("fancy_probe", p)
 //	pr, err := cloudprober.InitFromConfig(*configFile)
 //	if err != nil {
 //		log.Exitf("Error initializing cloudprober. Err: %v", err)
 //	}
-func Register(name string, probe Probe) {
+func RegisterUserDefined(name string, probe Probe) {
 	userDefinedProbesMu.Lock()
 	defer userDefinedProbesMu.Unlock()
 	userDefinedProbes[name] = probe
+}
+
+// RegisterProbeType registers a new probe-type. New probe types are integrated
+// with the config subsystem using the protobuf extensions.
+//
+// TODO: Add a full example of using extensions.
+func RegisterProbeType(extensionFieldNo int, newProbeFunc func() Probe) {
+	extensionMapMu.Lock()
+	defer extensionMapMu.Unlock()
+	extensionMap[extensionFieldNo] = newProbeFunc
 }
