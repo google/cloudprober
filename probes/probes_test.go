35d2e5f8ab495823d5d673cfd801aeb69f87aa6a
Manu Garg <manugarg@gmail.com>
2018-02-12 09:59:53 -0800

    Fix probes and targets tests.
    Make probes and targets tests more blackbox-y to avoid cyclic build dependency on "testdata". probes (and targets) tests use testdata package which in turn uses probes packages (to get the protos to extend), creating a cycling build dependency[1].
    
    Even apart from this, unless it makes things too complicated, it's better to just test interfaces of the packages. Go's standard library makes use both approaches.
    
    This CL also changes the following:
    = targets.New() interface now takes ldLister as a param.
    = in case of an error, probes.Init() should return an error instead of exiting.
    
    [1] - https://travis-ci.org/google/cloudprober/jobs/339338643
    
    ORIGINAL_AUTHOR=Manu Garg <manugarg@gmail.com>
    PiperOrigin-RevId: 185392871

--- probes/probes_test.go
+++ probes/probes_test.go
@@ -12,30 +12,44 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package probes
+package probes_test
 
 import (
 	"context"
 	"testing"
 
 	"github.com/golang/protobuf/proto"
+	"github.com/google/cloudprober/logger"
 	"github.com/google/cloudprober/metrics"
+	"github.com/google/cloudprober/probes"
 	"github.com/google/cloudprober/probes/options"
 	"github.com/google/cloudprober/probes/testdata"
+	"github.com/google/cloudprober/targets"
 )
+
+var testProbeIntialized int
 
 type testProbe struct{}
 
-func (p *testProbe) Init(name string, opts *options.Options) error { return nil }
+func (p *testProbe) Init(name string, opts *options.Options) error {
+	testProbeIntialized++
+	return nil
+}
 
 func (p *testProbe) Start(ctx context.Context, dataChan chan *metrics.EventMetrics) {}
 
 func TestGetExtensionProbe(t *testing.T) {
-	probeDef := &probes.ProbeDef{}
+	probeDef := &probes.ProbeDef{
+		Name: proto.String("ext-probe"),
+		Type: probes.ProbeDef_EXTENSION.Enum(),
+		Targets: &targets.TargetsDef{
+			Type: &targets.TargetsDef_DummyTargets{},
+		},
+	}
 
 	// This has the same effect as using the following in your config:
 	// probe {
-	//    name: "run_fancy_probe_for_x"
+	//    name: "ext-probe"
 	//    targets: ...
 	//    ...
 	//    [cloudprober.probes.testdata.fancy_probe] {
@@ -46,25 +60,29 @@ func TestGetExtensionProbe(t *testing.T) {
 	if err != nil {
 		t.Fatalf("error setting up extension in test probe proto: %v", err)
 	}
-	probeEx, _, err := getExtensionProbe(probeDef)
+	probeMap, err := probes.Init([]*probes.ProbeDef{probeDef}, nil, &logger.Logger{}, make(map[string]string))
 	if err == nil {
-		t.Errorf("Expected error in building probe from extensions, got nil. probe: %v", probeEx)
+		t.Errorf("Expected error in building probe from extensions, got nil. Probes map: %v", probeMap)
 	}
+	t.Log(err.Error())
 
 	// Register our test probe type and try again.
-	RegisterProbeType(200, func() Probe {
+	probes.RegisterProbeType(200, func() probes.Probe {
 		return &testProbe{}
 	})
-	probeEx, value, err := getExtensionProbe(probeDef)
+
+	probeMap, err = probes.Init([]*probes.ProbeDef{probeDef}, nil, &logger.Logger{}, make(map[string]string))
 	if err != nil {
 		t.Errorf("Got error in building probe from extensions: %v", err)
 	}
-	_, ok := value.(*testdata.FancyProbe)
-	if !ok {
-		t.Errorf("Extension probe conf (%v) is not of type *testdata.FancyProbe", value)
+	if probeMap["ext-probe"] == nil {
+		t.Errorf("Extension probe not in the probes map")
 	}
-	_, ok = probeEx.(*testProbe)
+	_, ok := probeMap["ext-probe"].(*testProbe)
 	if !ok {
-		t.Errorf("Extension probe (%v) is not of type *testProbe", probeEx)
+		t.Errorf("Extension probe (%v) is not of type *testProbe", probeMap["ext-probe"])
+	}
+	if testProbeIntialized != 1 {
+		t.Errorf("Extensions probe's Init() called %d times, should be called exactly once.", testProbeIntialized)
 	}
 }
