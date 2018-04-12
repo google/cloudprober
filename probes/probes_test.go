9e77c330878486f431bf456cd47d579fbd2041cf
Manu Garg <manugarg@gmail.com>
2018-04-12 14:30:48 -0700

    Move protos to their own packages - surfacers.
    This will simplify the internal <-> github conversion maintenance. Also:
    = It will reduces the chances of surprising name collisions that we have deal with now, after migrations to Github.
    = Autogenned protobuf Go files won't pollute the package documentation.
    
    ORIGINAL_AUTHOR=Manu Garg <manugarg@gmail.com>
    PiperOrigin-RevId: 192670938

--- probes/probes_test.go
+++ probes/probes_test.go
@@ -24,7 +24,7 @@ import (
 	"github.com/google/cloudprober/probes"
 	"github.com/google/cloudprober/probes/options"
 	configpb "github.com/google/cloudprober/probes/proto"
-	"github.com/google/cloudprober/probes/testdata"
+	testdatapb "github.com/google/cloudprober/probes/testdata"
 	targetspb "github.com/google/cloudprober/targets/proto"
 )
 
@@ -57,7 +57,7 @@ func TestGetExtensionProbe(t *testing.T) {
 	//      name: "fancy"
 	//    }
 	// }
-	err := proto.SetExtension(probeDef, testdata.E_FancyProbe, &testdata.FancyProbe{Name: proto.String("fancy")})
+	err := proto.SetExtension(probeDef, testdatapb.E_FancyProbe, &testdatapb.FancyProbe{Name: proto.String("fancy")})
 	if err != nil {
 		t.Fatalf("error setting up extension in test probe proto: %v", err)
 	}
