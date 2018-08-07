5227dcc475a4ac0d272740e7a98e42231d399a38
Manu Garg <manugarg@gmail.com>
2018-08-07 12:33:09 -0700

    Update protobufs' Go code.

--- probes/external/proto/server.pb.go
+++ probes/external/proto/server.pb.go
@@ -3,14 +3,20 @@
 
 package proto
 
-import proto1 "github.com/golang/protobuf/proto"
+import proto "github.com/golang/protobuf/proto"
 import fmt "fmt"
 import math "math"
 
 // Reference imports to suppress errors if they are not otherwise used.
-var _ = proto1.Marshal
+var _ = proto.Marshal
 var _ = fmt.Errorf
 var _ = math.Inf
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the proto package it is being compiled against.
+// A compilation error at this line likely means your copy of the
+// proto package needs to be updated.
+const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
 // ProbeRequest is the message that cloudprober sends to the external probe
 // server.
@@ -22,15 +28,36 @@ type ProbeRequest struct {
 	// milliseconds.  If the time limit is exceeded, the server
 	// should abort the request, but *not* send back a reply.  The
 	// client will have to do timeouts anyway.
-	TimeLimit        *int32                 `protobuf:"varint,2,req,name=time_limit,json=timeLimit" json:"time_limit,omitempty"`
-	Options          []*ProbeRequest_Option `protobuf:"bytes,3,rep,name=options" json:"options,omitempty"`
-	XXX_unrecognized []byte                 `json:"-"`
+	TimeLimit            *int32                 `protobuf:"varint,2,req,name=time_limit,json=timeLimit" json:"time_limit,omitempty"`
+	Options              []*ProbeRequest_Option `protobuf:"bytes,3,rep,name=options" json:"options,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}               `json:"-"`
+	XXX_unrecognized     []byte                 `json:"-"`
+	XXX_sizecache        int32                  `json:"-"`
 }
 
-func (m *ProbeRequest) Reset()                    { *m = ProbeRequest{} }
-func (m *ProbeRequest) String() string            { return proto1.CompactTextString(m) }
-func (*ProbeRequest) ProtoMessage()               {}
-func (*ProbeRequest) Descriptor() ([]byte, []int) { return fileDescriptor1, []int{0} }
+func (m *ProbeRequest) Reset()         { *m = ProbeRequest{} }
+func (m *ProbeRequest) String() string { return proto.CompactTextString(m) }
+func (*ProbeRequest) ProtoMessage()    {}
+func (*ProbeRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_server_5881812bd539f9a5, []int{0}
+}
+func (m *ProbeRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_ProbeRequest.Unmarshal(m, b)
+}
+func (m *ProbeRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_ProbeRequest.Marshal(b, m, deterministic)
+}
+func (dst *ProbeRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ProbeRequest.Merge(dst, src)
+}
+func (m *ProbeRequest) XXX_Size() int {
+	return xxx_messageInfo_ProbeRequest.Size(m)
+}
+func (m *ProbeRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_ProbeRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_ProbeRequest proto.InternalMessageInfo
 
 func (m *ProbeRequest) GetRequestId() int32 {
 	if m != nil && m.RequestId != nil {
@@ -54,15 +81,36 @@ func (m *ProbeRequest) GetOptions() []*ProbeRequest_Option {
 }
 
 type ProbeRequest_Option struct {
-	Name             *string `protobuf:"bytes,1,req,name=name" json:"name,omitempty"`
-	Value            *string `protobuf:"bytes,2,req,name=value" json:"value,omitempty"`
-	XXX_unrecognized []byte  `json:"-"`
+	Name                 *string  `protobuf:"bytes,1,req,name=name" json:"name,omitempty"`
+	Value                *string  `protobuf:"bytes,2,req,name=value" json:"value,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *ProbeRequest_Option) Reset()                    { *m = ProbeRequest_Option{} }
-func (m *ProbeRequest_Option) String() string            { return proto1.CompactTextString(m) }
-func (*ProbeRequest_Option) ProtoMessage()               {}
-func (*ProbeRequest_Option) Descriptor() ([]byte, []int) { return fileDescriptor1, []int{0, 0} }
+func (m *ProbeRequest_Option) Reset()         { *m = ProbeRequest_Option{} }
+func (m *ProbeRequest_Option) String() string { return proto.CompactTextString(m) }
+func (*ProbeRequest_Option) ProtoMessage()    {}
+func (*ProbeRequest_Option) Descriptor() ([]byte, []int) {
+	return fileDescriptor_server_5881812bd539f9a5, []int{0, 0}
+}
+func (m *ProbeRequest_Option) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_ProbeRequest_Option.Unmarshal(m, b)
+}
+func (m *ProbeRequest_Option) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_ProbeRequest_Option.Marshal(b, m, deterministic)
+}
+func (dst *ProbeRequest_Option) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ProbeRequest_Option.Merge(dst, src)
+}
+func (m *ProbeRequest_Option) XXX_Size() int {
+	return xxx_messageInfo_ProbeRequest_Option.Size(m)
+}
+func (m *ProbeRequest_Option) XXX_DiscardUnknown() {
+	xxx_messageInfo_ProbeRequest_Option.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_ProbeRequest_Option proto.InternalMessageInfo
 
 func (m *ProbeRequest_Option) GetName() string {
 	if m != nil && m.Name != nil {
@@ -91,14 +139,35 @@ type ProbeReply struct {
 	// var1 value1 (for example: total_errors 589)
 	// TODO(manugarg): Add an option to export mapped variables, for example:
 	// client-errors map:lang java:200 python:20 golang:3
-	Payload          *string `protobuf:"bytes,3,opt,name=payload" json:"payload,omitempty"`
-	XXX_unrecognized []byte  `json:"-"`
+	Payload              *string  `protobuf:"bytes,3,opt,name=payload" json:"payload,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *ProbeReply) Reset()                    { *m = ProbeReply{} }
-func (m *ProbeReply) String() string            { return proto1.CompactTextString(m) }
-func (*ProbeReply) ProtoMessage()               {}
-func (*ProbeReply) Descriptor() ([]byte, []int) { return fileDescriptor1, []int{1} }
+func (m *ProbeReply) Reset()         { *m = ProbeReply{} }
+func (m *ProbeReply) String() string { return proto.CompactTextString(m) }
+func (*ProbeReply) ProtoMessage()    {}
+func (*ProbeReply) Descriptor() ([]byte, []int) {
+	return fileDescriptor_server_5881812bd539f9a5, []int{1}
+}
+func (m *ProbeReply) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_ProbeReply.Unmarshal(m, b)
+}
+func (m *ProbeReply) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_ProbeReply.Marshal(b, m, deterministic)
+}
+func (dst *ProbeReply) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ProbeReply.Merge(dst, src)
+}
+func (m *ProbeReply) XXX_Size() int {
+	return xxx_messageInfo_ProbeReply.Size(m)
+}
+func (m *ProbeReply) XXX_DiscardUnknown() {
+	xxx_messageInfo_ProbeReply.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_ProbeReply proto.InternalMessageInfo
 
 func (m *ProbeReply) GetRequestId() int32 {
 	if m != nil && m.RequestId != nil {
@@ -122,16 +191,16 @@ func (m *ProbeReply) GetPayload() string {
 }
 
 func init() {
-	proto1.RegisterType((*ProbeRequest)(nil), "cloudprober.ProbeRequest")
-	proto1.RegisterType((*ProbeRequest_Option)(nil), "cloudprober.ProbeRequest.Option")
-	proto1.RegisterType((*ProbeReply)(nil), "cloudprober.ProbeReply")
+	proto.RegisterType((*ProbeRequest)(nil), "cloudprober.ProbeRequest")
+	proto.RegisterType((*ProbeRequest_Option)(nil), "cloudprober.ProbeRequest.Option")
+	proto.RegisterType((*ProbeReply)(nil), "cloudprober.ProbeReply")
 }
 
 func init() {
-	proto1.RegisterFile("github.com/google/cloudprober/probes/external/proto/server.proto", fileDescriptor1)
+	proto.RegisterFile("github.com/google/cloudprober/probes/external/proto/server.proto", fileDescriptor_server_5881812bd539f9a5)
 }
 
-var fileDescriptor1 = []byte{
+var fileDescriptor_server_5881812bd539f9a5 = []byte{
 	// 252 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x84, 0x8e, 0x31, 0x6b, 0xc3, 0x30,
 	0x10, 0x85, 0x71, 0xdc, 0x34, 0xf8, 0x92, 0x2e, 0xa2, 0x83, 0x29, 0x14, 0x4c, 0xba, 0x78, 0xb2,
