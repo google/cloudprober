9382a5a2548c478785437af56cb5c692d7cba9ef
Manu Garg <manugarg@gmail.com>
2018-10-16 14:59:36 -0700

    Add support for writing compressed data through FILE surfacer.
    PiperOrigin-RevId: 217392777

--- surfacers/file/file_test.go
+++ surfacers/file/file_test.go
@@ -31,11 +31,21 @@ import (
 	"github.com/golang/protobuf/proto"
 	"github.com/kylelemons/godebug/pretty"
 
+	"github.com/google/cloudprober/logger"
 	"github.com/google/cloudprober/metrics"
 	configpb "github.com/google/cloudprober/surfacers/file/proto"
 )
 
-func TestRun(t *testing.T) {
+func TestWrite(t *testing.T) {
+	testWrite(t, false)
+}
+
+func TestWriteWithCompression(t *testing.T) {
+	testWrite(t, true)
+}
+
+func testWrite(t *testing.T, compressionEnabled bool) {
+	t.Helper()
 
 	tests := []struct {
 		description string
@@ -62,10 +72,13 @@ func TestRun(t *testing.T) {
 		defer os.Remove(f.Name())
 
 		s := &FileSurfacer{
-			c: &configpb.SurfacerConf{FilePath: proto.String(f.Name())},
+			c: &configpb.SurfacerConf{
+				FilePath:           proto.String(f.Name()),
+				CompressionEnabled: proto.Bool(compressionEnabled),
+			},
 		}
 		id := time.Now().UnixNano()
-		err = s.init(id)
+		err = s.init(context.Background(), id)
 		if err != nil {
 			t.Errorf("Unable to create a new file surfacer: %v\ntest description: %s", err, tt.description)
 		}
@@ -76,14 +89,61 @@ func TestRun(t *testing.T) {
 		// performing the write to finish writing to the file before
 		// we read (otherwise we will read too early and the file will
 		// be blank)
-		time.Sleep(100 * time.Millisecond)
+		// time.Sleep(100 * time.Millisecond)
+		time.Sleep(compressionBufferFlushInterval + time.Second)
 
 		dat, err := ioutil.ReadFile(f.Name())
 		if err != nil {
 			t.Errorf("Unable to open test output file for reading: %v\ntest description: %s", err, tt.description)
 		}
-		if diff := pretty.Compare(fmt.Sprintf("%s %d %s\n", s.c.GetPrefix(), id, tt.em.String()), string(dat)); diff != "" {
+
+		expectedStr := fmt.Sprintf("%s %d %s\n", s.c.GetPrefix(), id, tt.em.String())
+		if compressionEnabled {
+			compressed, err := compressBytes([]byte(expectedStr))
+			if err != nil {
+				t.Errorf("Unexpected error while compressing bytes: %v, Input: %s", err, expectedStr)
+			}
+			expectedStr = compressed + "\n"
+		}
+
+		if diff := pretty.Compare(expectedStr, string(dat)); diff != "" {
 			t.Errorf("Message written does not match expected output (-want +got):\n%s\ntest description: %s", diff, tt.description)
 		}
+	}
+}
+
+// Test that compress bytes is compressing bytes appropriately.
+func TestCompressBytes(t *testing.T) {
+	testString := "string that is about to be compressed"
+	compressed, err := compressBytes([]byte(testString))
+	if err != nil {
+		t.Errorf("Got error while compressing the test string (%s): %v", testString, err)
+	}
+	// Verified that following is a good string using:
+	// in="H4sIAAAAAAAA/youKcrMS1coyUgsUcgsVkhMyi8tUSjJV0hKVUjOzy0oSi0uTk0BBAAA//+2oNy3JQAAAA=="
+	// echo -n $in | base64 -d | gunzip -c
+	expectedCompressed := "H4sIAAAAAAAA/youKcrMS1coyUgsUcgsVkhMyi8tUSjJV0hKVUjOzy0oSi0uTk0BBAAA//+2oNy3JQAAAA=="
+	if compressed != expectedCompressed {
+		t.Errorf("compressBytes(), got=%s, expected=%s", compressed, expectedCompressed)
+	}
+}
+
+func TestCompressionBufferFlush(t *testing.T) {
+	c := &compressionBuffer{
+		outChan: make(chan string, 1000),
+		l:       &logger.Logger{},
+	}
+
+	// Write a test line to the buffer.
+	c.writeLine("test string")
+
+	if c.buf.Len() == 0 || c.lines == 0 {
+		t.Errorf("compressionBuffer unexpected empty")
+	}
+
+	c.flush()
+
+	if c.buf.Len() != 0 && c.lines != 0 {
+		t.Errorf("flush() didn't empty the compressionBuffer")
 	}
 }
