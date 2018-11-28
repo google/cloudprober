e54b83c81afbeeaadfbf875e89c74aff2d2b73af
Manu Garg <manugarg@gmail.com>
2018-11-28 15:56:47 -0800

    Add a status handler (/status) that shows configured probes, servers and surfacers.
    PiperOrigin-RevId: 223251593

--- web/web.go
+++ web/web.go
@@ -16,20 +16,126 @@
 package web
 
 import (
+	"bytes"
 	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/google/cloudprober"
 )
 
+const style = `
+<style type="text/css">
+table.status-list {
+  border-collapse: collapse;
+  border-spacing: 0;
+	margin-bottom: 40px;
+	font-family: monospace;
+}
+table.status-list td,th {
+  border: 1px solid gray;
+  padding: 0.25em 0.5em;
+	max-width: 200px;
+}
+pre {
+    white-space: pre-wrap;
+		word-wrap: break-word;
+}
+</style>`
+
+const probeStatusTmpl = `
+
+<h3>Probes:</h3>
+
+<table class="status-list">
+  <tr>
+    <th>Name</th>
+    <th>Type</th>
+    <th>Interval</th>
+    <th>Timeout</th>
+    <th>Targets</th>
+    <th>Probe Conf</th>
+    <th>Latency Unit</th>
+    <th>Latency Distribution Lower Bounds (if configured) </th>
+  </tr>
+  {{ range . }}
+  <tr>
+    <td>{{.Name}}</td>
+    <td>{{.Type}}</td>
+    <td>{{.Interval}}</td>
+    <td>{{.Timeout}}</td>
+    <td><pre>{{.TargetsDesc}}</pre></td>
+
+    <td>
+    {{if .ProbeConf}}
+      <pre>{{.ProbeConf}}</pre>
+    {{else}}
+      default
+    {{end}}
+    </td>
+
+    <td>{{.LatencyUnit}}</td>
+    <td><pre>{{.LatencyDistLB}}</pre></td>
+  </tr>
+  {{ end }}
+</table>`
+
+const surfacerStatusTmpl = `
+<table class="status-list">
+  <tr>
+    <th>Type</th>
+    <th>Name</th>
+    <th>Conf</th>
+  </tr>
+  {{ range . }}
+  <tr>
+    <td>{{.Type}}</td>
+    <td>{{.Name}}</td>
+    <td>
+    {{if .Conf}}
+      <pre>{{.Conf}}</pre>
+    {{else}}
+      default
+    {{end}}
+    </td>
+  </tr>
+  {{ end }}
+</table>`
+
+// Status returns cloudprober status string.
+func Status() string {
+	var statusBuf bytes.Buffer
+
+	statusBuf.WriteString(style)
+	probeInfo, surfacerInfo, serverInfo := cloudprober.GetInfo()
+
+	// Probes info
+	tmpl, _ := template.New("statusProber").Parse(probeStatusTmpl)
+	tmpl.Execute(&statusBuf, probeInfo)
+
+	// Surfacers info
+	statusBuf.WriteString("<h3>Surfacers</h3>")
+	tmpl, _ = template.New("statusSurfacer").Parse(surfacerStatusTmpl)
+	tmpl.Execute(&statusBuf, surfacerInfo)
+
+	// Servers info
+	statusBuf.WriteString("<h3>Servers</h3>")
+	tmpl, _ = template.New("statusServer").Parse(surfacerStatusTmpl)
+	tmpl.Execute(&statusBuf, serverInfo)
+
+	return statusBuf.String()
+}
+
 func configHandler(w http.ResponseWriter, r *http.Request) {
-	config := cloudprober.GetConfig()
-	fmt.Fprintf(w, config)
+	fmt.Fprintf(w, cloudprober.GetConfig())
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, Status())
 }
 
 // Init initializes cloudprober web interface handler.
 func Init() {
 	http.HandleFunc("/config", configHandler)
-	// TODO(manugarg): Add a status handler that shows the running probes and
-	// their configs.
+	http.HandleFunc("/status", statusHandler)
 }
