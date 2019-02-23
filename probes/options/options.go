dddf6c9001eb2538ff46dc005cf4eb73bd5ea74a
Manu Garg <manugarg@gmail.com>
2019-02-22 22:42:59 -0800

    Only refactoring: move options building to "probes/options" package from "probes".
    Also, move newLogger function to the "logger" package with the name "NewCloudproberLog" to avoid duplication.
    
    PiperOrigin-RevId: 235312074

--- probes/options/options.go
+++ probes/options/options.go
@@ -18,12 +18,17 @@ Package options provides a shared interface to common probe options.
 package options
 
 import (
+	"fmt"
 	"net"
 	"time"
 
 	"github.com/google/cloudprober/logger"
 	"github.com/google/cloudprober/metrics"
+	"github.com/google/cloudprober/probes/probeutils"
+	configpb "github.com/google/cloudprober/probes/proto"
 	"github.com/google/cloudprober/targets"
+	"github.com/google/cloudprober/targets/lameduck"
+	targetspb "github.com/google/cloudprober/targets/proto"
 	"github.com/google/cloudprober/validators"
 )
 
@@ -37,4 +42,70 @@ type Options struct {
 	LatencyUnit       time.Duration
 	Validators        []*validators.ValidatorWithName
 	SourceIP          net.IP
+}
+
+// getSourceFromConfig returns the source IP from the config either directly
+// or by resolving the network interface to an IP, depending on which is provided.
+func getSourceIPFromConfig(p *configpb.ProbeDef, l *logger.Logger) (net.IP, error) {
+	switch p.SourceIpConfig.(type) {
+
+	case *configpb.ProbeDef_SourceIp:
+		sourceIP := net.ParseIP(p.GetSourceIp())
+		if sourceIP == nil {
+			return nil, fmt.Errorf("invalid source IP: %s", p.GetSourceIp())
+		}
+		return sourceIP, nil
+
+	case *configpb.ProbeDef_SourceInterface:
+		return probeutils.ResolveIntfAddr(p.GetSourceInterface())
+
+	default:
+		return nil, fmt.Errorf("unknown source type: %v", p.GetSourceIpConfig())
+	}
+}
+
+// BuildProbeOptions builds probe's options using the provided config and some global params.
+func BuildProbeOptions(p *configpb.ProbeDef, ldLister lameduck.Lister, globalTargetsOpts *targetspb.GlobalTargetsOptions, l *logger.Logger) (*Options, error) {
+	opts := &Options{
+		Interval: time.Duration(p.GetIntervalMsec()) * time.Millisecond,
+		Timeout:  time.Duration(p.GetTimeoutMsec()) * time.Millisecond,
+	}
+
+	var err error
+	if opts.Logger, err = logger.NewCloudproberLog(p.GetName()); err != nil {
+		return nil, fmt.Errorf("error in initializing logger for the probe (%s): %v", p.GetName(), err)
+	}
+
+	if opts.Targets, err = targets.New(p.GetTargets(), ldLister, globalTargetsOpts, l, opts.Logger); err != nil {
+		return nil, err
+	}
+
+	if latencyDist := p.GetLatencyDistribution(); latencyDist != nil {
+		var d *metrics.Distribution
+		if d, err = metrics.NewDistributionFromProto(latencyDist); err != nil {
+			return nil, fmt.Errorf("error creating distribution from the specification (%v): %v", latencyDist, err)
+		}
+		opts.LatencyDist = d
+	}
+
+	// latency_unit is specified as a human-readable string, e.g. ns, ms, us etc.
+	if opts.LatencyUnit, err = time.ParseDuration("1" + p.GetLatencyUnit()); err != nil {
+		return nil, fmt.Errorf("failed to parse the latency unit (%s): %v", p.GetLatencyUnit(), err)
+	}
+
+	if len(p.GetValidator()) > 0 {
+		opts.Validators, err = validators.Init(p.GetValidator(), opts.Logger)
+		if err != nil {
+			return nil, fmt.Errorf("failed to initialize validators: %v", err)
+		}
+	}
+
+	if p.GetSourceIpConfig() != nil {
+		opts.SourceIP, err = getSourceIPFromConfig(p, l)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get source address for the probe: %v", err)
+		}
+	}
+
+	return opts, nil
 }
