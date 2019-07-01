68c08b7d5f8492f9fc7bce64e38e405468dd7b1f
Manu Garg <manugarg@gmail.com>
2019-06-30 21:41:30 -0700

    Fix a config bug in HTTP probe initialization.
    Check for StatsExportIntervalMsec in config only if a config is supplied, i.e., p.c is nil.
    
    PiperOrigin-RevId: 255885876

--- probes/http/http.go
+++ probes/http/http.go
@@ -151,7 +151,7 @@ func (p *Probe) Init(name string, opts *options.Options) error {
 		Transport: transport,
 	}
 
-	if p.c.StatsExportIntervalMsec != nil {
+	if p.c != nil && p.c.StatsExportIntervalMsec != nil {
 		p.l.Warningf("stats_export_interval_msec field is now deprecated and doesn't do anything. To modify stats export interval, use the probe level field by the same name.")
 	}
 
