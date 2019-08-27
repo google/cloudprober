55c38c46aee1fbad830f8f321afc8873d75186f2
Manu Garg <manugarg@gmail.com>
2019-08-27 10:32:07 -0700

    Implement gce_targets { instances{} } targets using RDS backend.
    Instead of creating a GCE instances provider per project, we'll create an RDS server and client pair per project. Client will directly call ListResources on the server, instead of communicating over gRPC (thus saving on the protobuf marshaling/unmarshaling costs).
    
    This change removes the duplicate implementation of fetching GCE instances list.
    
    PiperOrigin-RevId: 265714029

--- targets/gce/gce.go
+++ targets/gce/gce.go
@@ -48,20 +48,33 @@
 package gce
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"net"
+	"sync"
 
 	"cloud.google.com/go/compute/metadata"
+	"github.com/golang/protobuf/proto"
 	"github.com/google/cloudprober/logger"
 	configpb "github.com/google/cloudprober/targets/gce/proto"
 	dnsRes "github.com/google/cloudprober/targets/resolver"
+
+	"github.com/google/cloudprober/targets/rds/client"
+	clientconfigpb "github.com/google/cloudprober/targets/rds/client/proto"
+	rdspb "github.com/google/cloudprober/targets/rds/proto"
+	"github.com/google/cloudprober/targets/rds/server"
+	gcpconfigpb "github.com/google/cloudprober/targets/rds/server/gcp/proto"
+	serverconfigpb "github.com/google/cloudprober/targets/rds/server/proto"
 )
 
-// Targets are able to list and resolve targets with their List and Resolve
-// methods.  A single instance of Targets represents a specific listing method
-// --- if multiple sets of resources need to be listed/resolved, a separate
-// instance of Targets will be needed.
+var global struct {
+	mu      sync.RWMutex
+	servers map[string]*server.Server
+}
+
+// Targets interface is redefined here (originally defined in targets.go) to
+// allow returning private gceResources.
 type Targets interface {
 	// List produces list of targets.
 	List() []string
@@ -70,8 +83,134 @@ type Targets interface {
 	Resolve(name string, ipVer int) (net.IP, error)
 }
 
+// gceResources encapsulates a set of GCE resources of a particular type. It's
+// mostly a wrapper around RDS clients created during the initialization step.
+type gceResources struct {
+	c          *configpb.TargetsConf
+	globalOpts *configpb.GlobalOptions
+	l          *logger.Logger
+
+	// RDS parameters
+	resourceType string
+	ipConfig     *rdspb.IPConfig
+	filters      []*rdspb.Filter
+	clients      map[string]*client.Client
+
+	// DNS config
+	r      *dnsRes.Resolver
+	useDNS bool
+}
+
+func initRDSServer(resourceType, apiVersion string, projects []string, reEvalInterval int32, l *logger.Logger) (*server.Server, error) {
+	global.mu.Lock()
+	defer global.mu.Unlock()
+
+	if global.servers == nil {
+		global.servers = make(map[string]*server.Server)
+	}
+
+	if global.servers[resourceType] != nil {
+		return global.servers[resourceType], nil
+	}
+
+	gcpConfig := &gcpconfigpb.ProviderConfig{
+		Project:    projects,
+		ApiVersion: &apiVersion,
+	}
+
+	if resourceType == "gce_instances" {
+		gcpConfig.GceInstances = &gcpconfigpb.GCEInstances{ReEvalSec: &reEvalInterval}
+	}
+
+	serverConf := &serverconfigpb.ServerConf{
+		Provider: []*serverconfigpb.Provider{
+			{
+				Id:     proto.String("gcp"),
+				Config: &serverconfigpb.Provider_GcpConfig{GcpConfig: gcpConfig},
+			},
+		},
+	}
+
+	srv, err := server.New(context.Background(), serverConf, nil, l)
+	if err != nil {
+		return nil, err
+	}
+
+	global.servers[resourceType] = srv
+	return srv, nil
+}
+
+func newRDSClient(req *rdspb.ListResourcesRequest, listResourcesFunc client.ListResourcesFunc, l *logger.Logger) (*client.Client, error) {
+	c := &clientconfigpb.ClientConf{
+		Request:   req,
+		ReEvalSec: proto.Int32(10), // Refresh client every 10s.
+	}
+
+	client, err := client.New(c, listResourcesFunc, l)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func (gr *gceResources) rdsRequest(project string) *rdspb.ListResourcesRequest {
+	return &rdspb.ListResourcesRequest{
+		Provider:     proto.String("gcp"),
+		ResourcePath: proto.String(fmt.Sprintf("%s/%s", gr.resourceType, project)),
+		Filter:       gr.filters,
+		IpConfig:     gr.ipConfig,
+	}
+}
+
+func (gr *gceResources) initClients(projects []string) error {
+	srv, err := initRDSServer(gr.resourceType, gr.globalOpts.GetApiVersion(), projects, gr.globalOpts.GetReEvalSec(), gr.l)
+	if err != nil {
+		return err
+	}
+
+	for _, project := range projects {
+		client, err := newRDSClient(gr.rdsRequest(project), srv.ListResources, gr.l)
+		if err != nil {
+			return err
+		}
+		gr.clients[project] = client
+	}
+
+	return nil
+}
+
+// List calls List() on the underlying RDS clients.
+func (gr *gceResources) List() []string {
+	var names []string
+	for _, client := range gr.clients {
+		names = append(names, client.List()...)
+	}
+	return names
+}
+
+// Resolve resolves the name into an IP address. Unless explicitly configured
+// to use DNS, we use the RDS client to determine the resource IPs.
+func (gr *gceResources) Resolve(name string, ipVer int) (net.IP, error) {
+	if gr.useDNS {
+		return gr.r.Resolve(name, ipVer)
+	}
+
+	var ip net.IP
+	var err error
+
+	for _, client := range gr.clients {
+		ip, err = client.Resolve(name, ipVer)
+		if err == nil {
+			return ip, err
+		}
+	}
+
+	return ip, err
+}
+
 // New is a helper function to unpack a Targets proto into a Targets interface.
-func New(conf *configpb.TargetsConf, opts *configpb.GlobalOptions, res *dnsRes.Resolver, log *logger.Logger) (t Targets, err error) {
+func New(conf *configpb.TargetsConf, globalOpts *configpb.GlobalOptions, res *dnsRes.Resolver, l *logger.Logger) (Targets, error) {
 	projects := conf.GetProject()
 	if projects == nil {
 		if !metadata.OnGCE() {
@@ -83,13 +222,40 @@ func New(conf *configpb.TargetsConf, opts *configpb.GlobalOptions, res *dnsRes.R
 			return nil, fmt.Errorf("targets.gce.New(): Error getting project ID: %v", err)
 		}
 	}
+
+	gr := &gceResources{
+		c:          conf,
+		ipConfig:   instancesIPConfig(conf.GetInstances()),
+		clients:    make(map[string]*client.Client),
+		globalOpts: globalOpts,
+		l:          l,
+	}
+
 	switch conf.Type.(type) {
 	case *configpb.TargetsConf_Instances:
-		t, err = newInstances(projects, opts, conf.GetInstances(), res, log)
+		gr.resourceType = "gce_instances"
+		// Verify that config is correct.
+		if err := verifyInstancesConfig(conf.GetInstances(), res); err != nil {
+			return nil, err
+		}
+
+		if len(conf.GetInstances().GetLabel()) > 0 {
+			filters, err := parseLabels(conf.GetInstances().GetLabel())
+			if err != nil {
+				return nil, err
+			}
+			gr.filters = filters
+		}
+		return gr, gr.initClients(projects)
+
 	case *configpb.TargetsConf_ForwardingRules:
-		t, err = newForwardingRules(projects[0], opts, conf.GetForwardingRules(), log)
-	default:
-		err = errors.New("unknown GCE targets type")
+		// TODO(manugarg): implement forwarding rules using gceResources like instances.go.
+		frp, err := newForwardingRules(projects[0], globalOpts, conf.GetForwardingRules(), l)
+		if err != nil {
+			return nil, err
+		}
+		return frp, nil
 	}
-	return
+
+	return nil, errors.New("unknown GCE targets type")
 }
