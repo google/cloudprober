9a93400ee12efe2097fea6edef5c7b989584003e
Didi Kohen <kohen.d@gmail.com>
2019-11-14 12:34:10 +0200

    Fix prints, add license, set boolean to true

--- sysvars/sysvars_ec2.go
+++ sysvars/sysvars_ec2.go
@@ -1,3 +1,16 @@
+// Copyright 2019 Didi Kohen
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package sysvars
 
 import (
@@ -11,8 +24,7 @@ import (
 func getEC2Meta(sysVars map[string]string) error {
 	ec2Session, err := session.NewSession()
 	if err != nil {
-		fmt.Printf("Could not create session %v", err)
-		return err
+		return fmt.Errorf("Could not create session %v", err)
 	}
 	ec2met := ec2metadata.New(ec2Session)
 	// Doing the availability check in module since we need a session
@@ -23,10 +35,11 @@ func getEC2Meta(sysVars map[string]string) error {
 
 	id, err := ec2met.GetInstanceIdentityDocument()
 	if err != nil {
-		fmt.Printf("Could not get instance document %v", err)
-		return err
+		sysVars["EC2_METADATA_Availabile"] = "false"
+		return fmt.Errorf("Could not get instance document %v", err)
 	}
 
+	sysVars["EC2_METADATA_Availabile"] = "true"
 	sysVars["EC2_DevpayProductCodes"] = strings.Join(id.DevpayProductCodes, ",")
 	sysVars["EC2_MarketplaceProductCodes"] = strings.Join(id.MarketplaceProductCodes, ",")
 	sysVars["EC2_AvailabilityZone"] = id.AvailabilityZone
