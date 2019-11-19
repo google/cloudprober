4bba03458dc6af27dbf4afaf7c323ae4a48751ad
Didi Kohen <kohen.d@gmail.com>
2019-11-18 22:34:57 -0800

    Add AWS EC2 metadata variables to sysvars.
    Related issue: https://github.com/google/cloudprober/issues/294
    
    PiperOrigin-RevId: 281226268

---
+++ sysvars/sysvars_ec2.go
@@ -0,0 +1,54 @@
+// Copyright 2019 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sysvars
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws/ec2metadata"
+	"github.com/aws/aws-sdk-go/aws/session"
+)
+
+func ec2Vars(sysVars map[string]string) error {
+	s, err := session.NewSession()
+	if err != nil {
+		return fmt.Errorf("ec2Vars: could not create session %v", err)
+	}
+
+	md := ec2metadata.New(s)
+	// Doing the availability check in module since we need a session
+	if md.Available() == false {
+		sysVars["EC2_METADATA_Available"] = "false"
+		return nil
+	}
+
+	id, err := md.GetInstanceIdentityDocument()
+	if err != nil {
+		sysVars["EC2_METADATA_Available"] = "false"
+		return fmt.Errorf("ec2Vars: could not get instance document %v", err)
+	}
+
+	sysVars["EC2_METADATA_Available"] = "true"
+	sysVars["EC2_AvailabilityZone"] = id.AvailabilityZone
+	sysVars["EC2_PrivateIP"] = id.PrivateIP
+	sysVars["EC2_Region"] = id.Region
+	sysVars["EC2_InstanceID"] = id.InstanceID
+	sysVars["EC2_InstanceType"] = id.InstanceType
+	sysVars["EC2_ImageID"] = id.ImageID
+	sysVars["EC2_KernelID"] = id.KernelID
+	sysVars["EC2_RamdiskID"] = id.RamdiskID
+	sysVars["EC2_Architecture"] = id.Architecture
+	return nil
+}
