8ff836675caf9dec0fcd071e045473fb5161c4eb
Robin McCorkell <rmccorkell@google.com>
2021-06-03 12:54:26 -0700

    Batch read/write UDP packets
    On Linux, ReadBatch/WriteBatch can reduce the number of syscalls needed,
    improving performance. On other platforms, these functions fallback to
    single-packet read/write, which is identical to the current behaviour.
    
    PiperOrigin-RevId: 377353137

--- servers/udp/udp.go
+++ servers/udp/udp.go
@@ -22,12 +22,12 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"runtime"
 	"strings"
 
 	"github.com/google/cloudprober/logger"
 	"github.com/google/cloudprober/metrics"
 	configpb "github.com/google/cloudprober/servers/udp/proto"
-	"golang.org/x/net/ipv4"
 	"golang.org/x/net/ipv6"
 )
 
@@ -36,6 +36,15 @@ const (
 	// the maximum allowed by the OS. 425984 is the max value in
 	// Container-Optimized OS version 9592.90.0.
 	readBufSize = 425984
+
+	// Number of messages to batch read.
+	batchSize = 16
+
+	// Maximum packet size.
+	// TODO(manugarg): We read and echo back only 4098 bytes. We should look at raising this
+	// limit or making it configurable. Also of note, ReadFromUDP reads a single UDP datagram
+	// (up to the max size of 64K-sizeof(UDPHdr)) and discards the rest.
+	maxPacketSize = 4098
 )
 
 // Server implements a basic UDP server.
@@ -44,10 +53,7 @@ type Server struct {
 	conn *net.UDPConn
 	l    *logger.Logger
 
-	// IPv4 and IPv6 connection types are initialized and used only on
-	// non-windows systems.
 	p6 *ipv6.PacketConn
-	p4 *ipv4.PacketConn
 }
 
 // New returns an UDP server.
@@ -65,9 +71,24 @@ func New(initCtx context.Context, c *configpb.ServerConf, l *logger.Logger) (*Se
 		c:    c,
 		conn: conn,
 		l:    l,
+
+		// We use an IPv6 connection wrapper to receive both IPv4 and IPv6 packets.
+		// ipv6.PacketConn lets us use control messages (non-Windows only) to:
+		//  -- receive packet destination IP (FlagDst)
+		//  -- set source IP (Src).
+		p6: ipv6.NewPacketConn(conn),
 	}
 
-	return s, s.initConnection()
+	switch runtime.GOOS {
+	case "windows":
+		// Control messages are not supported.
+	default:
+		if err := s.p6.SetControlMessage(ipv6.FlagDst, true); err != nil {
+			return nil, fmt.Errorf("SetControlMessage(ipv6.FlagDst, true) failed: %v", err)
+		}
+	}
+
+	return s, nil
 }
 
 // Listen opens a UDP socket on the given port. It also attempts to set recv
@@ -87,36 +108,22 @@ func Listen(addr *net.UDPAddr, l *logger.Logger) (*net.UDPConn, error) {
 	return conn, nil
 }
 
-func (s *Server) readPacket(buf []byte) (int, *ipv6.ControlMessage, net.Addr, error) {
-	// If protocol level packet connection is available (work only on
-	// non-windows), use low-level read.
-	if s.p6 == nil {
-		len, addr, err := s.conn.ReadFromUDP(buf)
-		return len, nil, addr, err
-	}
-	return s.p6.ReadFrom(buf)
+func (s *Server) readPackets(ms []ipv6.Message) (int, error) {
+	return s.p6.ReadBatch(ms, 0)
 }
 
-func (s *Server) writePacket(buf []byte, cm *ipv6.ControlMessage, addr net.Addr) (int, error) {
-	// If no control message (should happen only on windows), use high-level UDP
-	// write function.
-	if cm == nil {
-		return s.conn.WriteToUDP(buf, addr.(*net.UDPAddr))
-	}
-
-	// We have an IPv4 address.
-	if cm.Dst.To4() != nil {
-		wcm := &ipv4.ControlMessage{
-			Src: cm.Dst.To4(),
+func (s *Server) writePackets(ms []ipv6.Message) error {
+	for remaining := len(ms); remaining > 0; {
+		n, err := s.p6.WriteBatch(ms, 0)
+		if err != nil {
+			return fmt.Errorf("writing packets: %v", err)
 		}
-		return s.p4.WriteTo(buf, wcm, addr)
+		if n == 0 {
+			return fmt.Errorf("wrote zero packets, %d remain", remaining)
+		}
+		remaining -= n
 	}
-
-	// Assume IPv6 address.
-	wcm := &ipv6.ControlMessage{
-		Src: cm.Dst.To16(),
-	}
-	return s.p6.WriteTo(buf, wcm, addr)
+	return nil
 }
 
 // readAndEcho reads a packet from the server connection and writes it back. To
@@ -124,22 +131,40 @@ func (s *Server) writePacket(buf []byte, cm *ipv6.ControlMessage, addr net.Addr)
 // a load balancer), we make use of control messages (Note: this doesn't work
 // on Windows OS as Go doesn't provide a way to access control messages on
 // Windows).
-func (s *Server) readAndEcho(buf []byte) (error, error) {
-	inLen, cm, addr, err := s.readPacket(buf)
+//
+// Note that we don't need to copy or modify the messages below before echoing them
+// for the following reasons:
+//   - Message struct uses the same field (Addr) for sender (while receiving) and
+//     destination address (while sending).
+//   - Control message (type: packet-info) field that contains the received
+//     packet's destination address, is also the field that's used to set the source
+//     address on the outgoing packets.
+func (s *Server) readAndEcho(ms []ipv6.Message) error {
+	n, err := s.readPackets(ms)
 	if err != nil {
-		return fmt.Errorf("error reading from UDP: %v", err), err
+		return fmt.Errorf("error reading from UDP: %v", err)
+	}
+	ms = ms[:n]
+
+	// Resize buffers to match amount read.
+	for _, m := range ms {
+		// We only allocated a 0th buffer, so all the data is there.
+		m.Buffers[0] = m.Buffers[0][:m.N]
 	}
 
-	n, err := s.writePacket(buf[:inLen], cm, addr)
+	err = s.writePackets(ms)
 	if err != nil {
-		return fmt.Errorf("error writing to UDP: %v", err), err
+		return fmt.Errorf("error writing to UDP: %v", err)
 	}
 
-	if n < inLen {
-		s.l.Warningf("Reply truncated! Got %d bytes but only sent %d bytes", inLen, n)
+	// Reset buffers to full size for re-use.
+	for _, m := range ms {
+		b := m.Buffers[0]
+		// We only allocated a 0th buffer.
+		m.Buffers[0] = b[:cap(b)]
 	}
 
-	return nil, nil
+	return nil
 }
 
 func connClosed(err error) bool {
@@ -150,10 +175,16 @@ func connClosed(err error) bool {
 
 // Start starts the UDP server. It returns only when context is canceled.
 func (s *Server) Start(ctx context.Context, dataChan chan<- *metrics.EventMetrics) error {
-	// TODO(manugarg): We read and echo back only 4098 bytes. We should look at raising this
-	// limit or making it configurable. Also of note, ReadFromUDP reads a single UDP datagram
-	// (up to the max size of 64K-sizeof(UDPHdr)) and discards the rest.
-	buf := make([]byte, 4098)
+	ms := make([]ipv6.Message, batchSize)
+	for i := 0; i < batchSize; i++ {
+		ms[i].Buffers = [][]byte{make([]byte, maxPacketSize)}
+		switch runtime.GOOS {
+		case "linux":
+			ms[i].OOB = ipv6.NewControlMessage(ipv6.FlagDst)
+		default:
+			// Control messages are not supported.
+		}
+	}
 
 	// Setup a background function to close connection if context is canceled.
 	// Typically, this is not what we want (close something started outside of
@@ -169,8 +200,8 @@ func (s *Server) Start(ctx context.Context, dataChan chan<- *metrics.EventMetric
 	case configpb.ServerConf_ECHO:
 		s.l.Infof("Starting UDP ECHO server on port %d", int(s.c.GetPort()))
 		for {
-			if err, nestedErr := s.readAndEcho(buf); err != nil {
-				if connClosed(nestedErr) {
+			if err := s.readAndEcho(ms); err != nil {
+				if connClosed(err) {
 					s.l.Warning("connection closed, stopping the start goroutine")
 					return nil
 				}
@@ -181,7 +212,7 @@ func (s *Server) Start(ctx context.Context, dataChan chan<- *metrics.EventMetric
 	case configpb.ServerConf_DISCARD:
 		s.l.Infof("Starting UDP DISCARD server on port %d", int(s.c.GetPort()))
 		for {
-			if _, _, err := s.conn.ReadFromUDP(buf); err != nil {
+			if _, err := s.readPackets(ms); err != nil {
 				if connClosed(err) {
 					return nil
 				}
