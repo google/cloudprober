7a89856aefd3dc3d23094bfd04482c275bd36364
Manu Garg <manugarg@gmail.com>
2021-06-04 15:23:07 -0700

    Fix UDP server code for Windows.
    We still need to use single packet read-write functions for Windows. ReadBatch and WriteBatch still use recvmmsg/recvmsg behind the scene, which is not implemented on Windows. It fails with the following error:
    
    "E0603 21:05:17.372650    6892 udp.go:208] [] error reading from UDP: read udp [::]:53793: recvmsg: not implemented on windows/amd64"
    
    https://github.com/google/cloudprober/runs/2740869679?check_suite_focus=true
    
    PiperOrigin-RevId: 377601248

--- servers/udp/udp.go
+++ servers/udp/udp.go
@@ -20,6 +20,7 @@ package udp
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net"
 	"runtime"
@@ -53,7 +54,8 @@ type Server struct {
 	conn *net.UDPConn
 	l    *logger.Logger
 
-	p6 *ipv6.PacketConn
+	advancedReadWrite bool // Set to true on non-windows systems
+	p6                *ipv6.PacketConn
 }
 
 // New returns an UDP server.
@@ -71,18 +73,18 @@ func New(initCtx context.Context, c *configpb.ServerConf, l *logger.Logger) (*Se
 		c:    c,
 		conn: conn,
 		l:    l,
-
-		// We use an IPv6 connection wrapper to receive both IPv4 and IPv6 packets.
-		// ipv6.PacketConn lets us use control messages (non-Windows only) to:
-		//  -- receive packet destination IP (FlagDst)
-		//  -- set source IP (Src).
-		p6: ipv6.NewPacketConn(conn),
 	}
 
 	switch runtime.GOOS {
 	case "windows":
 		// Control messages are not supported.
 	default:
+		s.advancedReadWrite = true
+		// We use an IPv6 connection wrapper to receive both IPv4 and IPv6 packets.
+		// ipv6.PacketConn lets us use control messages (non-Windows only) to:
+		//  -- receive packet destination IP (FlagDst)
+		//  -- set source IP (Src).
+		s.p6 = ipv6.NewPacketConn(conn)
 		if err := s.p6.SetControlMessage(ipv6.FlagDst, true); err != nil {
 			return nil, fmt.Errorf("SetControlMessage(ipv6.FlagDst, true) failed: %v", err)
 		}
@@ -108,41 +110,36 @@ func Listen(addr *net.UDPAddr, l *logger.Logger) (*net.UDPConn, error) {
 	return conn, nil
 }
 
-func (s *Server) readPackets(ms []ipv6.Message) (int, error) {
-	return s.p6.ReadBatch(ms, 0)
+// readWriteErr is used by readAndEcho functions so that we can return both,
+// the relevant error message and the original error. Original error is used
+// to determine if the underlying transport has been closed.
+type readWriteErr struct {
+	msg string
+	err error
 }
 
-func (s *Server) writePackets(ms []ipv6.Message) error {
-	for remaining := len(ms); remaining > 0; {
-		n, err := s.p6.WriteBatch(ms, 0)
-		if err != nil {
-			return fmt.Errorf("writing packets: %v", err)
-		}
-		if n == 0 {
-			return fmt.Errorf("wrote zero packets, %d remain", remaining)
-		}
-		remaining -= n
+func (rwerr *readWriteErr) Error() string {
+	if rwerr.err != nil {
+		return fmt.Sprintf("%s: %v", rwerr.msg, rwerr.err)
 	}
-	return nil
+	return rwerr.msg
 }
 
-// readAndEcho reads a packet from the server connection and writes it back. To
-// determine the source address for outgoing packets (e.g. if server is behind
-// a load balancer), we make use of control messages (Note: this doesn't work
-// on Windows OS as Go doesn't provide a way to access control messages on
-// Windows).
+// readAndEchoBatch reads, writes packets in batches. To determine the source
+// address for outgoing packets (e.g. if server is behind a load balancer), we
+// make use of control messages (configured through messages' OOB).
 //
-// Note that we don't need to copy or modify the messages below before echoing them
-// for the following reasons:
-//   - Message struct uses the same field (Addr) for sender (while receiving) and
-//     destination address (while sending).
+// Note that we don't need to copy or modify the messages below before echoing
+// them for the following reasons:
+//   - Message struct uses the same field (Addr) for sender (while receiving)
+//     and destination address (while sending).
 //   - Control message (type: packet-info) field that contains the received
-//     packet's destination address, is also the field that's used to set the source
-//     address on the outgoing packets.
-func (s *Server) readAndEcho(ms []ipv6.Message) error {
-	n, err := s.readPackets(ms)
+//     packet's destination address, is also the field that's used to set the
+//     source address on the outgoing packets.
+func (s *Server) readAndEchoBatch(ms []ipv6.Message) *readWriteErr {
+	n, err := s.p6.ReadBatch(ms, 0)
 	if err != nil {
-		return fmt.Errorf("error reading from UDP: %v", err)
+		return &readWriteErr{"error reading packets", err}
 	}
 	ms = ms[:n]
 
@@ -152,9 +149,15 @@ func (s *Server) readAndEcho(ms []ipv6.Message) error {
 		m.Buffers[0] = m.Buffers[0][:m.N]
 	}
 
-	err = s.writePackets(ms)
-	if err != nil {
-		return fmt.Errorf("error writing to UDP: %v", err)
+	for remaining := len(ms); remaining > 0; {
+		n, err := s.p6.WriteBatch(ms, 0)
+		if err != nil {
+			return &readWriteErr{"error writing packets", err}
+		}
+		if n == 0 {
+			return &readWriteErr{fmt.Sprintf("wrote zero packets, %d remain", remaining), nil}
+		}
+		remaining -= n
 	}
 
 	// Reset buffers to full size for re-use.
@@ -167,6 +170,28 @@ func (s *Server) readAndEcho(ms []ipv6.Message) error {
 	return nil
 }
 
+// readAndEchoSimple reads a packet from the server connection and writes it
+// back.
+func (s *Server) readAndEchoSimple(buf []byte) *readWriteErr {
+	inLen, addr, err := s.conn.ReadFromUDP(buf)
+	if err != nil {
+		return &readWriteErr{"error reading packet", err}
+	}
+	if inLen == 0 {
+		return &readWriteErr{"read 0 length packet", nil}
+	}
+
+	n, err := s.conn.WriteToUDP(buf[:inLen], addr)
+	if err != nil {
+		return &readWriteErr{"error writing packet", err}
+	}
+
+	if n < inLen {
+		s.l.Warningf("Reply truncated! Got %d bytes but only sent %d bytes", inLen, n)
+	}
+	return nil
+}
+
 func connClosed(err error) bool {
 	// TODO(manugarg): Replace this by errors.Is(err, net.ErrClosed) once Go 1.16
 	// is more widely available.
@@ -175,14 +200,14 @@ func connClosed(err error) bool {
 
 // Start starts the UDP server. It returns only when context is canceled.
 func (s *Server) Start(ctx context.Context, dataChan chan<- *metrics.EventMetrics) error {
-	ms := make([]ipv6.Message, batchSize)
-	for i := 0; i < batchSize; i++ {
-		ms[i].Buffers = [][]byte{make([]byte, maxPacketSize)}
-		switch runtime.GOOS {
-		case "linux":
+	var ms []ipv6.Message              // Used for batch read-write
+	buf := make([]byte, maxPacketSize) // Used for single packet read-write (windows)
+
+	if s.advancedReadWrite {
+		ms = make([]ipv6.Message, batchSize)
+		for i := 0; i < batchSize; i++ {
+			ms[i].Buffers = [][]byte{make([]byte, maxPacketSize)}
 			ms[i].OOB = ipv6.NewControlMessage(ipv6.FlagDst)
-		default:
-			// Control messages are not supported.
 		}
 	}
 
@@ -199,21 +224,36 @@ func (s *Server) Start(ctx context.Context, dataChan chan<- *metrics.EventMetric
 
 	case configpb.ServerConf_ECHO:
 		s.l.Infof("Starting UDP ECHO server on port %d", int(s.c.GetPort()))
+
+		var rwerr *readWriteErr
 		for {
-			if err := s.readAndEcho(ms); err != nil {
-				if connClosed(err) {
+			if s.advancedReadWrite {
+				rwerr = s.readAndEchoBatch(ms)
+			} else {
+				rwerr = s.readAndEchoSimple(buf)
+			}
+			if rwerr != nil {
+				if errors.Is(rwerr.err, net.ErrClosed) {
 					s.l.Warning("connection closed, stopping the start goroutine")
 					return nil
 				}
-				s.l.Error(err.Error())
+				s.l.Error(rwerr.Error())
 			}
 		}
 
 	case configpb.ServerConf_DISCARD:
 		s.l.Infof("Starting UDP DISCARD server on port %d", int(s.c.GetPort()))
+
+		var err error
 		for {
-			if _, err := s.readPackets(ms); err != nil {
-				if connClosed(err) {
+			if s.advancedReadWrite {
+				_, err = s.p6.ReadBatch(ms, 0)
+			} else {
+				_, _, err = s.conn.ReadFromUDP(buf)
+			}
+
+			if err != nil {
+				if errors.Is(err, net.ErrClosed) {
 					return nil
 				}
 				s.l.Errorf("ReadFromUDP: %v", err)
